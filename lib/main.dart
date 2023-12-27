212f6fc0d8a1d5434ebfc17d237c43b52401c73b
Chirag Bhatia <bhatiachirag2002@gmail.com>
2023-12-27 11:54:59 +0530

    Project Created

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:weather_sphere/view/home.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(const WeatherSphere());
+}
+
+class WeatherSphere extends StatelessWidget {
+  const WeatherSphere({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: false,
+      builder: (context, child) {
+        return const MaterialApp(
+            debugShowCheckedModeBanner: false, home: HomeView());
+      },
+    );
+  }
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color gradientColor1 = const Color(0xff232526);
+  static Color gradientColor2 = const Color(0xff414345);
+  static Color weatherContainer = const Color(0xff1a1e1e);
+  static Color textColor = const Color(0xffffffff);
+  static Color divederColor = const Color(0xFF686868);
+}
